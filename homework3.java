6cb0b472cb95ef13248fe7429a2ea099c14cd37c
abdulahi3 <abdulahiyasin409@gmail.com>
2025-06-17 08:54:31 -0900

    Add files via upload

---
+++ homework3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class homework3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String Text = input.nextLine();
+
+        int first = 0;
+
+        int second = Text.length() - 1;
+
+        boolean isPalindrome = true;
+        while (first < second) {
+            if (Text.charAt(first) != Text.charAt(second)) {
+                isPalindrome = false;
+                break;
+            }
+
+            first++;
+            second--;
+        }
+
+        if (isPalindrome)
+            System.out.println(Text + " is a palindrome");
+        else
+            System.out.println(Text + " is not a palindrome");
+    }
+}
